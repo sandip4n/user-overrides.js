cb1dffb82d081aaac7ba0dabff2a4c69a2cf97bb
Sandipan Das <sandipan.osd@gmail.com>
2023-08-22 07:58:23 +0000

    user-overrides: initial commit
    Signed-off-by: Sandipan Das <sandipan.osd@gmail.com>

---
+++ user-overrides.js
@@ -0,0 +1,60 @@
+/* For Section 0100: STARTUP */
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+
+/* For Section 0300: QUIETER FOX */
+user_pref("app.feedback.baseURL", "");
+user_pref("app.normandy.first_run", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.partnerlink.attributionURL", "");
+user_pref("browser.partnerlink.campaign.topsites", "");
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
+
+/* For Section 0800: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS */
+user_pref("browser.urlbar.suggest.engines", false);
+
+/* For Section 0900: PASSWORDS */
+user_pref("signon.firefoxRelay.feature", "disabled");
+user_pref("signon.generation.enabled", false);
+user_pref("signon.management.page.breach-alerts.enabled", false);
+user_pref("signon.rememberSignons", false);
+
+/* For Section 1600: HEADERS / REFERERS */
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+
+/* For Section 2600: MISCELLANEOUS */
+user_pref("accessibility.typeaheadfind.flashBar", 0);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.bookmarks.editDialog.confirmationHintShowCount", 2);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.showMobileBookmarks", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("permissions.default.camera", 2);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.xr", 2);
+
+/* For Section 2800: SHUTDOWN & SANITIZING */
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.cpd.cache", true);
+user_pref("privacy.cpd.formdata", true);
+user_pref("privacy.cpd.history", true);
+user_pref("privacy.cpd.sessions", true);
+user_pref("privacy.cpd.offlineApps", true);
+user_pref("privacy.cpd.cookies", true);
+user_pref("privacy.cpd.downloads", true);
+user_pref("privacy.cpd.openWindows", true);
+user_pref("privacy.cpd.passwords", true);
+user_pref("privacy.cpd.siteSettings", true);
+user_pref("privacy.sanitize.timeSpan", 0);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("privacy.history.custom", true);
